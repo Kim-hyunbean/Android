1d0e00dab5fbb3c2b68a14f8efffc0dfd6116e92
Kim-hyunbean <95971047+Kim-hyunbean@users.noreply.github.com>
2021-12-29 23:44:09 +0900

    Add files via upload

---
+++ Ex27/MainActivity.java
@@ -0,0 +1,74 @@
+package com.cookandroid.ex2_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText text1;
+    Button bt1, bt2;
+    RadioGroup Rg;
+    RadioButton Rb1, Rb2;
+    ImageView image1 ;
+//    final String address = text1.getText().toString();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text1 = (EditText) findViewById(R.id.text1);
+        bt1 = (Button) findViewById(R.id.bt1);
+        bt2 = (Button) findViewById(R.id.bt2);
+        Rg = (RadioGroup) findViewById(R.id.Rg);
+        Rb1 = (RadioButton) findViewById(R.id.Rb1);
+        Rb2 = (RadioButton) findViewById(R.id.Rb2);
+        image1 = (ImageView) findViewById(R.id.image1);
+
+
+//        bt1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), address,
+//                        Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//
+//        bt2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent netlntent = new Intent(Intent.ACTION_VIEW,
+//                        Uri.parse(address));
+//                startActivity(netlntent);
+//
+//            }
+//        });
+
+        switch (Rg.getCheckedRadioButtonId()) {
+            case R.id.Rb1:
+                image1.setImageResource(R.drawable.image4);
+                break;
+
+            case R.id.Rb2:
+                image1.setImageResource(R.drawable.image1);
+                break;
+
+            default:
+        }
+
+
+
+    }
+}
